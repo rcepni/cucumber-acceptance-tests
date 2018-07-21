d15eb196cf55d92f1c930ea5470b0d587f488e8c
CybertekSchool <github@cybertekschool.com>
2018-07-21 14:53:03 -0400

    added step defs file

---
+++ src/test/java/com/prestashop/step_definitions/HomePageStepDefs.java
@@ -0,0 +1,28 @@
+package com.prestashop.step_definitions;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class HomePageStepDefs {
+
+	@Given("the user is on the home page")
+	public void the_user_is_on_the_home_page() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@When("the user enters a search term")
+	public void the_user_enters_a_search_term() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@Then("the search box should contain the search term")
+	public void the_search_box_should_contain_the_search_term() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+}
