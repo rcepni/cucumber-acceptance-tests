d1bbead55557d3a6b2d4462a60016909063d043a
CybertekSchool <github@cybertekschool.com>
2018-07-21 14:07:24 -0400

    added missing files

---
+++ src/test/java/com/prestashop/pages/SigninPage.java
@@ -0,0 +1,26 @@
+package com.prestashop.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.prestashop.utilities.Driver;
+
+public class SigninPage {
+	public SigninPage() {
+		PageFactory.initElements(Driver.getDriver(), this);
+	}
+
+	@FindBy(id = "email_create")
+	public WebElement signupEmail;
+	
+	@FindBy(id = "email")
+	public WebElement loginEmail;
+	
+	@FindBy(id = "password")
+	public WebElement password;
+	
+	@FindBy(id = "submitLogin")
+	public WebElement submitLogin;
+	
+}
