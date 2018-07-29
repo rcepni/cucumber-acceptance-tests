70f4a7f8c4fa576a4ff7895defc3fb66683b1441
marufjon <marufjon@192.168.1.162>
2018-07-29 10:13:00 -0400

    added user information

--- src/test/java/com/prestashop/step_definitions/DataTableConfigurer.java
+++ src/test/java/com/prestashop/step_definitions/DataTableConfigurer.java
@@ -10,12 +10,11 @@ import cucumber.api.TypeRegistryConfigurer;
 import io.cucumber.datatable.DataTableType;
 import io.cucumber.datatable.TableEntryTransformer;
 
-
 /*
- * THis class needs to be in same package as step deifnitons
- * it will map the datatable to our custom object
+ * THis class needs to be in same package as step definitions
+ * it will map the data table to our custom object
  */
-public class DataTableConfigurer implements TypeRegistryConfigurer{
+public class DataTableConfigurer implements TypeRegistryConfigurer {
 
 	@Override
 	public Locale locale() {
@@ -33,10 +32,16 @@ public class DataTableConfigurer implements TypeRegistryConfigurer{
 						// these keys must match the data table in the feature file
 						String firstName = row.get("First Name");
 						String lastName = row.get("Last Name");
-						String address = row.get("Address");
-						String city = row.get("City");
 						String company = row.get("Company");
-						return new User(firstName, lastName, address, company, city);
+						String email = row.get("Email");
+						String password = row.get("Password");
+						String address = row.get("Address");
+						String zipcode = row.get("Zipcode");
+						String state = row.get("State");
+						String phone = row.get("Phone");
+						String city = row.get("City");
+						return new User(firstName, lastName, email, phone, company, address, city, password, zipcode,
+								state);
 					}
 				}));
 
@@ -47,7 +52,7 @@ public class DataTableConfigurer implements TypeRegistryConfigurer{
 //	                Price.fromString(row.get("price"))
 //	            )
 //	        ));
-		
+
 	}
 
 }
